252c9cf88c86516f23c5f30a8027f426dd3ff8c6
MEL55555 <p010519p@student.staffs.ac.uk>
2026-03-04 16:27:49 +0000

    RED RECTANGLE

--- Graphics_Programming/HelloGL.cpp
+++ Graphics_Programming/HelloGL.cpp
@@ -8,10 +8,14 @@ void HelloGL::Display()
 {
 
 	glClear(GL_COLOR_BUFFER_BIT); //this clears the scene glBegin(GL_POLYGON); //starts to draw a polygon
-	glVertex2f(-0.75, 0.5); //define the first point of the polygon, top left glVertex2f(0.75, 0.5); //next point, top right
-	glVertex2f(0.75, -0.5); //bottom right
-	glVertex2f(-0.75, -0.5); //last point of the polygon, bottom left
+	
+	glBegin(GL_POLYGON); 
+		glVertex2f(-0.75, 0.5); //define the first point of the polygon, top left glVertex2f(0.75, 0.5); //next point, top right
+		glVertex2f(0.75f, 0.5f); 
+		glVertex2f(0.75, -0.5); //bottom right
+		glVertex2f(-0.75, -0.5); //last point of the polygon, bottom left
 	glEnd(); // defines the end of the draw
+
 	glFlush(); //flushes the scene drawn to the graphics card
 }
 
@@ -19,7 +23,10 @@ HelloGL::HelloGL(int argc, char* argv[])
 {
 	GLUTCallbacks::Init(this);
 	glutInit(&argc, argv);
-	glutCreateWindow("Simple OpenGL Program"); 
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutCreateWindow("Simple OpenGL Program");
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // white background
+	glColor3f(1.0f, 0.0f, 0.0f);          // red rectangle
 	glutDisplayFunc(GLUTCallbacks::Display);
 	glutMainLoop();
 }
